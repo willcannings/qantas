f319e8de1f0c774da0fb031cff998001c65b0c52
Will Cannings <me@willcannings.com>
2015-05-31 02:09:44 +1000

    More work

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(function() {
+    $('section article > div > div').each(function(i, el) {
+        var el = $(el);
+        var height = el.height() + 30;
+        el.parent().css({height: height + 'px'});
+    });
+
     $('.tasks article h1 div').click(function(event) {
         $(this).toggleClass('complete');
         event.stopImmediatePropagation();
